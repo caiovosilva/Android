6a528b3c90e23ede7e857462b969b26cca600c72
Caio Silva <caiovosilva@outlook.com>
2018-07-12 16:50:58 -0300

    atividade simples, contem PopUp

---
+++ Calculadora IMC/app/src/main/java/com/ifba/caiovosilva/calculadoraimc/MainActivity.java
@@ -0,0 +1,58 @@
+package com.ifba.caiovosilva.calculadoraimc;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnCalcularOnClick(View view){
+        EditText peso =(EditText) findViewById (R.id.txtPeso);
+        double valorPeso = Double.parseDouble(peso.getText().toString());
+        EditText altura = (EditText) findViewById(R.id.txtAltura);
+        double valorAltura = Double.parseDouble(altura.getText().toString());
+        showPopUp(calculoIMC(valorPeso,valorAltura));
+    }
+
+    private String calculoIMC(double peso, double altura){
+        double result = peso/(altura*altura);
+        if(result<19)
+            return "Magro(a) demais!";
+        if(result>32)
+            return "Gordo(a) demais!";
+        return "Você tá de boa.";
+    }
+
+    private void showPopUp(String msg){
+        AlertDialog alerta;
+         //Cria o gerador do AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //define o titulo
+        builder.setTitle("Resultado");
+        //define a mensagem
+        builder.setMessage(msg);
+        //define o botao OK
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                Toast.makeText(MainActivity.this, "OK!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //cria o AlertDialog
+        alerta = builder.create();
+        //Exibe
+        alerta.show();
+
+    }
+
+}
